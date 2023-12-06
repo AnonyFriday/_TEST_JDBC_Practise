8a0970a980c7b7f978b66f8109e2bb8c8a2f2255
AnonyFriday <duyvukim752000@gmail.com>
2023-12-06 16:23:27 +0700

    Create the boilerplate code for interacting JDBC with SQL Server

--- src/main/java/database/DBUtils.java
+++ src/main/java/database/DBUtils.java
@@ -10,69 +10,66 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * @author duyvu
  */
 public final class DBUtils {
 
-    // =================================
-    // == SQL Server
-    // =================================
-    /**
-     * Make the Connection to the SQL Server
-     *
-     * @return
-     */
-    public static Connection connectSQLServer() {
-        Connection conn = null;
-        try {
-            final String dbURL = """
-                                jdbc:sqlserver://localhost:1433;\
-                                databaseName=Bookstore;\
-                                encrypt=true;\
-                                trustServerCertificate=true;""";
-            final String username = "sa";
-            final String password = "334/26/30a";
+	// =================================
+	// == SQL Server
+	// =================================
+	/**
+	 * Make the Connection to the SQL Server
+	 *
+	 * @return
+	 */
+	public static Connection connectSQLServer() {
+		Connection conn = null;
+		try {
+			final String dbURL    = """
+					jdbc:sqlserver://localhost:1433;\
+					databaseName=Bookstore;\
+					encrypt=true;\
+					trustServerCertificate=true;""";
+			final String username = "sa";
+			final String password = "334/26/30a";
 
-            conn = DriverManager.getConnection(dbURL, username, password);
-        } catch (final SQLException ex) {
-            ex.printStackTrace();
-        }
-        return conn;
-    }
+			conn = DriverManager.getConnection(dbURL, username, password);
+		} catch (final SQLException ex) {
+			ex.printStackTrace();
+		}
+		return conn;
+	}
 
-    /**
-     * Method to close the connection on SQL Server
-     *
-     * @param conn
-     */
-    public static void closeConnectionSQLServer(final Connection conn) {
-        try {
-            if (conn == null || conn.isClosed()) {
-                return;
-            }
-            conn.close();
-        } catch (final SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
+	/**
+	 * Method to close the connection on SQL Server
+	 *
+	 * @param conn
+	 */
+	public static void closeConnectionSQLServer(final Connection conn) {
+		try {
+			if (conn == null || conn.isClosed())
+				return;
+			conn.close();
+		} catch (final SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
 
-    // =================================
-    // == MySQL
-    // =================================
-    // =================================
-    // == General Methods
-    // =================================
-    public static void printDBServerInfo(Connection conn) {
-        try {
-            final DatabaseMetaData dm = conn.getMetaData();
-            System.out.println("Driver name: " + dm.getDriverName());
-            System.out.println("Driver version: " + dm.getDriverVersion());
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
+	// =================================
+	// == MySQL
+	// =================================
+	// =================================
+	// == General Methods
+	// =================================
+	public static void printDBServerInfo(final Connection conn) {
+		try {
+			final DatabaseMetaData dm = conn.getMetaData();
+			System.out.println("Driver name: " + dm.getDriverName());
+			System.out.println("Driver version: " + dm.getDriverVersion());
+		} catch (final SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
--- src/test/java/TestDBUtils.java
+++ src/test/java/TestDBUtils.java
@@ -1,6 +1,10 @@
 
 import database.DBUtils;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -13,8 +17,34 @@ import java.sql.Connection;
 public class TestDBUtils {
 
     public static void main(String[] args) {
-        Connection conn = DBUtils.connectSQLServer();
-        DBUtils.printDBServerInfo(conn);
-        DBUtils.closeConnectionSQLServer(conn);
+
+        try {
+            /**
+             * Follow 5 step to interact with JDBC with SQL Server
+             */
+
+            // Step 1: Connect to the sql server
+            Connection conn = DBUtils.connectSQLServer();
+            DBUtils.printDBServerInfo(conn);
+
+            // Step 2: Create Statement, ResultSet(optional)
+            Statement st = conn.createStatement();
+
+            // Step 3: Create the sql query string and execute
+            String sql = """
+                        insert into Book
+                        values (1234567891234, 'Don''t be stupid like that', 'Paulo Coelho', 2, 2099);
+                        """;
+
+            int check = st.executeUpdate(sql);
+
+            // Step 4: Handle the returned value
+            System.out.println("Records changed: " + check);
+
+            // Step 5: Close the connection
+            DBUtils.closeConnectionSQLServer(conn);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
