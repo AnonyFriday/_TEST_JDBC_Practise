cd20f90b0dbfbebcd2b2d4c263854f39f2a05474
AnonyFriday <duyvukim752000@gmail.com>
2023-12-06 18:13:43 +0700

    Create the Customer model

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author duyvu
+ */
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Customer {
+
+    private String customerID;
+    private String fullName;
+    private Date dob;
+    private String address;
+
+}
